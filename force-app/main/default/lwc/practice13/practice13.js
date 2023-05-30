610a4aa101171dd0211235999ae9c6171d5de846
Sergey <sergey.zakharchenko@live.com>
2023-05-30 11:04:40 -0400

    LWC Practice

--- force-app/main/default/lwc/practice13/practice13.js
+++ force-app/main/default/lwc/practice13/practice13.js
@@ -1,6 +1,26 @@
+
+// -------------- 5/30/2023 -------- Practice 13 ----------------------
+
+
 import { LightningElement } from 'lwc';
 
 export default class Practice13 extends LightningElement {
 
-    
+        salary = 140000 ;
+
+        handleSalaryChange(event) {
+
+            this.salary = event.target.value;
+        }
+
+        get notTooBad(){
+            return this.salary>=100000 
+            && this.salary <= 135000;
+        }
+
+        get averageSalary(){
+            return this.Salary > 135000 
+            && this.salary <= 175000;
+        }
+
 }

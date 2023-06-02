aef7ef8f6519129d054c81c8fedae19aa6cf1fb1
Sergey <sergey.zakharchenko@live.com>
2023-06-02 12:22:26 -0400

    LWC practice

--- force-app/main/default/lwc/practice4/practice4.js
+++ force-app/main/default/lwc/practice4/practice4.js
@@ -2,23 +2,25 @@ import { LightningElement } from 'lwc';
 
 export default class Practice4 extends LightningElement {
 
-    num1 = 0 ;
-    num2 = 0 ;
-
-    get additionResult(){
-        
-        return this.num1 + this.num2 ;
+    num1 = 0 ; 
+    num2 = 0;
+    
+    get additionResult() {
+        return this.num1 + this.num2; 
     }
 
-    handleNum1Change(event){
+
+    handleNum1Change(event) {
         // set the value of num1 to what user typed
-        this.num1 = parseInt(event.target.value);
-   }
+        // turn String input into the number so we can take arithmetic operation
+        this.num1 = parseFloat(event.target.value); 
+    }
 
-    handleNum2Change(event){
-        // set the value of num2 to what user tped
-        this.Num2 = parseInt(event.target.value);
-
+    handleNum2Change(event) {
+        // set the value of num2 to what user typed
+        // turn String input into the number so we can take arithmetic operation
+        this.num2 = parseFloat(event.target.value); 
+        
     }
 
 

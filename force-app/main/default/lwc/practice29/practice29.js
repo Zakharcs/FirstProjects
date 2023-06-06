5dea1c796d86ddb92a1287a5c71c8aab6934b10a
Sergey <sergey.zakharchenko@live.com>
2023-06-06 11:53:17 -0400

    LWC practice

---
+++ force-app/main/default/lwc/practice29/practice29.js
@@ -0,0 +1,39 @@
+
+// -------------------- Practice29 ------ 6/6/2023 ------------------
+// ------------- Life Cycle Hook  LWC PRACTICE DAY 9 ----------------------
+
+
+import { LightningElement } from "lwc";
+export default class Practice29 extends LightningElement {
+
+
+    showSpinner ;
+
+
+
+    constructor() {
+        super(); 
+        console.log('message from constructor');
+        this.showSpinner = true ;
+    }
+
+
+    connectedCallback() {
+        console.log('message from connectedCallback method');
+        // wait 3 seconds here then make showSpinner value to false
+        // eslint-disable-next-line @lwc/lwc/no-async-operation
+        setTimeout(() => {
+            console.log('done waiting 3 seconds');
+            this.showSpinner = false;
+        }, 3000);
+    }
+
+
+    renderedCallback() {
+        console.log('message from renderedCallback method');
+    }
+
+    
+
+
+}

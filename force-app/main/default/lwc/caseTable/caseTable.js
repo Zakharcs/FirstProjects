cd91a1e2b21a719a0d058bdbd215cf0bd9824c78
Sergey <sergey.zakharchenko@live.com>
2023-06-14 16:19:04 -0400

    LWC caseTable

---
+++ force-app/main/default/lwc/caseTable/caseTable.js
@@ -0,0 +1,26 @@
+import get5CasesByStatus from "@salesforce/apex/CaseController.get5CasesByStatus";
+import { LightningElement, api, wire } from "lwc";
+export default class CaseTable extends LightningElement {
+    
+    // this public property is meant to filled up 
+    // by the app page while adding from app builder
+    // it has 4 options specified in xml Closed, New, Working, Escalated 
+    @api selectedStatus;
+    
+    columns = [
+        { label: 'Case Number', fieldName: 'CaseNumber' },
+        { label: 'Subject', fieldName: 'Subject' },
+        { label: 'Priority', fieldName: 'Priority'},
+        { label: 'Status', fieldName: 'Status'}
+    ];
+
+    @wire(get5CasesByStatus, {'statusParam' : '$selectedStatus' })
+    myCases;
+
+    // dynamically generate card title according to the status
+    get cardTitle(){
+
+        return this.selectedStatus + " Cases Table"
+    }
+
+}
